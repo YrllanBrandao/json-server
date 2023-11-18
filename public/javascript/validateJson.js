1e852a11e37d2983891a5450163e7b1516594e56
yrllanbrandao <yrllanbrandao@outlook.com>
2023-11-18 16:41:05 -0300

    feat: validate json

---
+++ public/javascript/validateJson.js
@@ -0,0 +1,34 @@
+const jsonTextArea = document.getElementById("jsonInput");
+const formJson = document.getElementById("form-json");
+
+
+formJson.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const isValid = validateJson();
+
+    if (isValid) {
+        alert("Válido")
+    }
+    else {
+        alert("não válido")
+    }
+})
+
+function validateJson() {
+    const jsonValue = jsonTextArea.value;
+
+    try {
+
+        if (jsonValue === '' || jsonValue === undefined) {
+            throw new Error();
+        }
+
+        const stringfiedJson = JSON.parse(jsonValue);
+        return true;
+    }
+    catch (error) {
+        return false;
+
+    }
+}
