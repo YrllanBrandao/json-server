52fd5f8b2bc4444bcccb9a9fe9270f7097a704a5
yrllanbrandao <yrllanbrandao@outlook.com>
2023-11-19 15:14:41 -0300

    feat: sanitize the param before call the json

--- App/Controllers/JsonController.php
+++ App/Controllers/JsonController.php
@@ -1,15 +1,28 @@
 <?php
 
-    namespace App\Controllers;
-    use MF\Controller\Action;
-    use App\Models\JsonModel;
-    
-    class JsonController extends Action {
-        
-        function saveJson(){
-            
+namespace App\Controllers;
+
+use App\Models\JsonModel;
+use MF\Controller\Action;
+
+class JsonController extends Action
+{
+
+    public function saveJson()
+    {
+
         $jsonModel = new JsonModel;
-                $jsonReceived = $_POST['jsonValue'];
-            $jsonModel -> saveJson($jsonReceived);
-        }
+        $jsonReceived = $_POST['jsonValue'];
+        $jsonModel->saveJson($jsonReceived);
     }
+    public function showJson($path)
+    {
+        $jsonModel = new JsonModel;
+
+
+
+        $sanitizedPath = str_replace('"', '', $path);
+        
+        $jsonModel->getJson($sanitizedPath);
+    }
+}
