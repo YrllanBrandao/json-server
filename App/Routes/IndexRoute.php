7b43051067922ba625da97e4fb48c6eedfb12e10
yrllanbrandao <yrllanbrandao@outlook.com>
2023-11-19 14:40:23 -0300

    feat: adding saved paths as route

--- App/Routes/IndexRoute.php
+++ App/Routes/IndexRoute.php
@@ -1,19 +1,40 @@
-<?php 
-    namespace App\Routes;
+<?php
+namespace App\Routes;
 
-    use App\Routes\AbstractRoute;
-    class IndexRoute extends abstractRoute{
-        private $routes;
-    
-        public function setRoutes(){
-            $routes['saveJson'] = [
-                'action' => 'saveJson',
-                'controller' => 'JsonController',
-                'route'=> '/save-json'
-            ];
-            $this -> routes =  $routes;
+use App\Models\JsonModel;
+use App\Routes\AbstractRoute;
+
+class IndexRoute extends abstractRoute
+{
+    private $routes;
+
+    public function setRoutes()
+    {
+
+        $jsonModel = new JsonModel;
+
+        $routes['saveJson'] = [
+            'action' => 'saveJson',
+            'controller' => 'JsonController',
+            'route' => '/save-json',
+        ];
+
+        $savedPaths = $jsonModel->getSavedPaths();
+
+        if ($savedPaths['route' !== '/404']) {
+            foreach($savedPaths as $paths){
+                $routes[$paths['path']] = [
+                    'route' => '/' . $paths['path'],
+                    'controller' => 'jsonController',
+                    'action' => 'showJson("' . $paths['path'] . '")'
+                ];
+            }
         }
-        public function getRoutes(){
-            return $this -> routes;
-        }
+
+        $this->routes = $routes;
     }
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+}
