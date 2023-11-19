24453b1be3489685c1f643853fabca23f1806d8e
yrllanbrandao <yrllanbrandao@outlook.com>
2023-11-19 14:00:15 -0300

    feat: verify if the generated path already exists

--- App/Models/JsonModel.php
+++ App/Models/JsonModel.php
@@ -2,6 +2,7 @@
 namespace App\Models;
 
 use App\Database\Connection;
+use \PDO;
 use \PDO\PDOException;
 
 class JsonModel
@@ -12,45 +13,68 @@ class JsonModel
         try {
             $jsonEncoded = json_encode($json);
             $sql = "
-                INSERT INTO registers(json) VALUES (:json)
+                INSERT INTO registers(json, path) VALUES (:json, :path)
                 ";
 
             $conn = new Connection;
             $connection = $conn->getConnection();
             $statement = $connection->prepare($sql);
-
+            $generatedPath = $this->generatePath();
             $statement->bindParam(':json', $jsonEncoded);
+            $statement->bindParam(':path', $generatedPath);
 
             $statement->execute();
 
+            
             http_response_code(201);
 
         } catch (PDOException $error) {
             echo 'An error has ocurred during the DB; Connection!';
         }
     }
-    
+    function verifyIfPathExists($path)
+    {
+        $sql = '
+        SELECT * FROM registers WHERE path = :path
+        ';
+        $conn = new Connection;
+        $connection = $conn->getConnection();
+        $statement = $connection->prepare($sql);
+
+        $statement->bindParam(':path', $path);
+
+        $statement->execute();
+
+        $queryResult = $statement->fetch(PDO::FETCH_ASSOC);
+
+        return !empty($queryResult);
+    }
     function generatePath()
     {
         $alphabet = 'abcdefghijklmnopqrstuvwxyz';
         $numbers = '0123456789';
         $path = '';
-        for ($i = 0; $i < 6; $i++) {
-            $randomCharacter = rand(0, 1);
-
-            switch ($randomCharacter) {
-                case 0:
-                    $indexAlphabet = rand(0, 25);
-                    $path .= $alphabet[$indexAlphabet];
-
-                    break;
-                case 1:
-                    $indexNumbers = rand(0, 9);
-                    $path .= $numbers[$indexNumbers];
-                    break;
+        $pathExists;
+        do{
+            for ($i = 0; $i < 6; $i++) {
+                $randomCharacter = rand(0, 1);
+    
+                switch ($randomCharacter) {
+                    case 0:
+                        $indexAlphabet = rand(0, 25);
+                        $path .= $alphabet[$indexAlphabet];
+    
+                        break;
+                    case 1:
+                        $indexNumbers = rand(0, 9);
+                        $path .= $numbers[$indexNumbers];
+                        break;
+                }
             }
-        }
-
+            
+            $pathExists = $this -> verifyIfPathExists($path);
+        }while($pathExists);
+        
         return $path;
     }
 
